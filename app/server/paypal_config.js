d9bcaa02053d38d27a499fabf6835d0e015aac4f
Ian Macleish <Ian.Macleish@akqa.com>
2015-03-01 06:43:51 -0800

    Adding some paypal stuff

---
+++ app/server/paypal_config.js
@@ -0,0 +1,6 @@
+Meteor.Paypal.config({
+  'host': 'api.sandbox.paypal.com',
+  'port': '',
+  'client_id': 'AXlWKBYTIW_KvZ32xvV1BZMEzgNrVDi_unuu6M7q4LZicVVp0BPk0loYnt1BXwb10GL1Y5PbMw3q7ogq',
+  'client_secret': 'EF0nq-F-pVjpxHI0NpRmadwUxGcrgNWdXGkV5-afCS589SfF8wGDqs506UpXY-EJZRp6nbemEe2UXJnC'
+});
